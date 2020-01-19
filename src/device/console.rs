ef45fad221c14e723f6f51711650a5d8469eabcf
Moritz Doll <doll@uni-bremen.de>
2020-01-20 00:12:49 +0100

    added reexport of fmt::Write in console

--- src/device/console.rs
+++ src/device/console.rs
@@ -1,4 +1,7 @@
 //! Defines a standard interface for TextIO, for instance MiniUart, Uart, and Debug
+
+pub use core::fmt::Write;
+
 pub trait Console {
     fn getc(&self) -> char;
     //fn flush(&mut self);
