abec5b5292bd6d0c4747b5069e558f8db3fc3b81
Moritz Doll <doll@uni-bremen.de>
2020-01-27 00:22:28 +0100

    added interrupt support for uart (untested)

--- src/device/uart.rs
+++ src/device/uart.rs
@@ -3,17 +3,39 @@
 // Author: Moritz Doll
 // License: MIT
 
-use core::ops;
-use core::fmt;
-use core::arch::arm;
-use register::{mmio::*, register_bitfields, Field};
 use crate::device::console;
-
+use core::arch::arm;
+use core::fmt;
+use core::ops;
+use register::{mmio::*, register_bitfields, Field};
 
 register_bitfields! {
     u32,
     DATA [
         DATA OFFSET(0) NUMBITS(8) []
+    ],
+    IER [
+        RHR OFFSET(0) NUMBITS(1) [Enable = 1, Disable = 0],
+        THR OFFSET(1) NUMBITS(1) [Enable = 1, Disable = 0],
+        LINESTS OFFSET(2) NUMBITS(1) [Enable = 1, Disable = 0],
+        MODEMSTS OFFSET(3) NUMBITS(1) [Enable = 1, Disable = 0],
+        SLEEPMODE OFFSET(4) NUMBITS(1) [Enable = 1, Disable = 0],
+        XOFF OFFSET(5) NUMBITS(1) [Enable = 1, Disable = 0],
+        RTS OFFSET(6) NUMBITS(1) [Enable = 1, Disable = 0],
+        CTS OFFSET(7) NUMBITS(1) [Enable = 1, Disable = 0]
+    ],
+    IIR [
+        PENDING OFFSET(0) NUMBITS(1) [Active = 1, NonActive = 0],
+        IT_TYPE OFFSET(1) NUMBITS(5) [
+            Modem = 0,
+            Thr = 1,
+            Rhr = 2,
+            LineStatus = 3,
+            RxTimeout = 6,
+            Xoff = 8,
+            Cts = 10
+        ],
+        FCR_MIRROR OFFSET(0) NUMBITS(2) []
     ],
     LSR [
         RXFIFOE OFFSET(0) NUMBITS(1) [NotEmpty = 0b1]
@@ -38,7 +60,7 @@ struct RegisterBlock {
     _MDR2: u32,
     __reserved_0: [u32; 6],
     _SCR: u32,
-    SSR: ReadOnly<u32, SSR::Register>
+    SSR: ReadOnly<u32, SSR::Register>,
 }
 
 struct MemoryUart {
@@ -48,13 +70,15 @@ struct MemoryUart {
 impl ops::Deref for MemoryUart {
     type Target = RegisterBlock;
     fn deref(&self) -> &Self::Target {
-        unsafe {&*self.ptr() }
+        unsafe { &*self.ptr() }
     }
 }
 
 impl MemoryUart {
     fn new(mem_addr: u32) -> MemoryUart {
-        MemoryUart {memory_addr: mem_addr }
+        MemoryUart {
+            memory_addr: mem_addr,
+        }
     }
     fn ptr(&self) -> *mut RegisterBlock {
         self.memory_addr as *mut _
@@ -68,7 +92,9 @@ pub struct Uart {
 impl Uart {
     pub fn new(address: u32) -> Uart {
         let memory_block = MemoryUart::new(address);
-        Uart { memory: memory_block }
+        Uart {
+            memory: memory_block,
+        }
     }
 
     #[inline]
